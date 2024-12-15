77f1a635041110ace473be6944d7a42762d326a0
irumaru <jibakomaorochi13@outlook.jp>
2024-12-15 05:24:27 +0000

    Add route controller

---
+++ config.boot
@@ -0,0 +1,90 @@
+interfaces {
+    ethernet eth0 {
+        address "192.168.10.11/24"
+        hw-id "bc:24:11:36:9f:e6"
+        offload {
+            gro
+            gso
+            sg
+            tso
+        }
+    }
+    ethernet eth1 {
+        address "192.168.9.1/24"
+        address "192.168.11.1/24"
+        hw-id "bc:24:11:fc:61:00"
+    }
+    ethernet eth2 {
+        hw-id "bc:24:11:71:04:1e"
+    }
+    ethernet eth3 {
+        hw-id "bc:24:11:de:09:54"
+    }
+    loopback lo {
+    }
+}
+protocols {
+    static {
+        route 0.0.0.0/0 {
+            next-hop 192.168.10.1 {
+            }
+        }
+    }
+}
+service {
+    ntp {
+        allow-client {
+            address "127.0.0.0/8"
+            address "169.254.0.0/16"
+            address "10.0.0.0/8"
+            address "172.16.0.0/12"
+            address "192.168.0.0/16"
+            address "::1/128"
+            address "fe80::/10"
+            address "fc00::/7"
+        }
+        server time1.vyos.net {
+        }
+        server time2.vyos.net {
+        }
+        server time3.vyos.net {
+        }
+    }
+    ssh {
+        port "22"
+    }
+}
+system {
+    config-management {
+        commit-revisions "100"
+    }
+    console {
+        device ttyS0 {
+            speed "115200"
+        }
+    }
+    host-name "vyos"
+    login {
+        user vyos {
+            authentication {
+                encrypted-password "$6$rounds=656000$GILz3ALYpxjfAsz7$BSFWyJu1y80eNTIx/xEejuV5dnCvmGa.n8N2UdnCS5HivFg/yDA6OKSTYX7Zs9H9xqPe5z5LHGYRUAkJqQ4cw1"
+                plaintext-password ""
+            }
+        }
+    }
+    syslog {
+        global {
+            facility all {
+                level "info"
+            }
+            facility local7 {
+                level "debug"
+            }
+        }
+    }
+}
+
+
+// Warning: Do not remove the following line.
+// vyos-config-version: "bgp@5:broadcast-relay@1:cluster@2:config-management@1:conntrack@5:conntrack-sync@2:container@2:dhcp-relay@2:dhcp-server@11:dhcpv6-server@6:dns-dynamic@4:dns-forwarding@4:firewall@17:flow-accounting@1:https@7:ids@1:interfaces@33:ipoe-server@4:ipsec@13:isis@3:l2tp@9:lldp@2:mdns@1:monitoring@1:nat@8:nat66@3:ntp@3:openconnect@3:openvpn@4:ospf@2:pim@1:policy@8:pppoe-server@11:pptp@5:qos@3:quagga@11:reverse-proxy@2:rip@1:rpki@2:salt@1:snmp@3:ssh@2:sstp@6:system@28:vrf@3:vrrp@4:vyos-accel-ppp@2:wanloadbalance@3:webproxy@2"
+// Release version: 1.5-rolling-202412060007
--- route-cmd.py
+++ route-cmd.py
@@ -5,22 +5,60 @@
 # 2. ゲートウェイのIPアドレス
 
 import json
+from io import StringIO
+import paramiko
+import vyos
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+updateConfigPath = "/config/config.update"
+saveConfigPath = "/config/config.boot"
+applyConfigCmd = """source /opt/vyatta/etc/functions/script-template
+configure
+load /config/config.update
+commit
+save
+"""
+
+hostname = "192.168.10.11"
+username = "vyos"
+password = "vyos"
+
+try:
+    client.connect(hostname=hostname, username=username, password=password)
+    print(f"{hostname}に接続しました。")
+except paramiko.AuthenticationException:
+    print("認証に失敗しました。ユーザー名とパスワードを確認してください。")
+except paramiko.SSHException as ssh_exception:
+    print(f"SSH接続エラー: {ssh_exception}")
 
 currentConfig = {
   # "dst": "gateway"
   "0.0.0.0/0": "192.168.10.1",
-  "135.1.0.15/32": "192.168.10.2"
+  "123.10.0.0/32": "192.168.10.2",
+  "15.0.0.1/32": "192.168.10.2"
 }
 
-env = "dev"
+env = "prod"
 devConfig = "route-config.json"
+
+def exec_command(command):
+  print(f"Executing command: {command}")
+  stdin, stdout, stderr = client.exec_command(command)
+  
+  status = stdout.channel.recv_exit_status()
+  print(f"Exit status: {status}")
+  print(f"STDOUT:\n{stdout.read().decode('utf-8')}\nSTDERR:\n{stderr.read().decode('utf-8')}")
+
+  return status
 
 # 経路更新
 # リストの更新を適用
 def UpdateRoute():
   config = getConfig()
 
-  newConfig = addRoute(config, currentConfig)
+  newConfig = replaceRoute(config, currentConfig)
 
   saveConfig(newConfig)
 
@@ -29,28 +67,30 @@ def getConfig():
   if env == "dev":
     with open(devConfig) as f:
       return json.load(f)
+  else:
+    with client.open_sftp() as sftp:
+      with sftp.open(saveConfigPath, "r") as file:
+        return file.read().decode('utf-8')
+
 
 # configへ経路を追加
-def addRoute(config, currentConfig):
-  # 経路のみ初期化
-  config["protocols"]["static"]["route"] = {}
+def replaceRoute(config, currentConfig):
+  pb = vyos.create_protocols_block(currentConfig)
 
-  # 経路追加
-  for key, value in currentConfig.items():
-    config["protocols"]["static"]["route"][key] = {
-      "next-hop": {
-        value: {}
-      }
-    }
-  
-  return config
+  return vyos.replace_protocols_block(config, pb)
 
 # config保存
 def saveConfig(config):
   # 設定のデバッグ
   if env == "dev":
     print(json.dumps(config, indent=4))
-  
-  # 設定書き込み
+  else:
+    # 設定ファイル書き込み
+    with client.open_sftp() as sftp:
+      with StringIO(config) as fileObj:
+        sftp.putfo(fileObj, updateConfigPath)
+    # 設定の適用
+    exec_command(applyConfigCmd)
 
-UpdateRoute()
+#UpdateRoute()
+print(UpdateRoute())
